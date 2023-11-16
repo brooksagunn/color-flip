21fad230b7df04be3cb304f7571a9ff4d1f68cf0
Brooks Gunn <brooks.a.gunn@gmail.com>
2023-11-16 16:13:50 -0500

    Add inital files and initial background color

---
+++ script.js
@@ -0,0 +1,22 @@
+const body = document.getElementById("main");
+
+const hexNums = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
+
+body.style.backgroundColor = "blue";
